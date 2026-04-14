bf813fd081d9aee7ca05f1daa1a281c716480223
Luis Pamplona <luispamplona2702@gmail.com>
2026-04-13 21:01:53 -0300

    feat : add a new page to add new subjects
    This PR create a new page that allow the user create new subjects

--- src/assets/routes/Index.tsx
+++ src/assets/routes/Index.tsx
@@ -3,6 +3,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "../pages/Register";
 import PrivateRoutes from "./PrivateRoutes";
 import Home from "../pages/Home";
+<<<<<<< Updated upstream:src/assets/routes/Index.tsx
+=======
+import Subjects from "../pages/subjects/Subjects";
+import AddSubject from "../pages/subjects/AddSubject";
+>>>>>>> Stashed changes:src/routes/Index.tsx
 
 export const AppRoutes = () => {
   return (
@@ -15,6 +20,11 @@ export const AppRoutes = () => {
           <Route element={<PrivateRoutes />}>
             <Route path="/home" element={<Home />} />
             <Route path="*" element={<Home />} />
+<<<<<<< Updated upstream:src/assets/routes/Index.tsx
+=======
+            <Route path="/subjects" element={<Subjects />} />
+            <Route path="/add-subject" element={<AddSubject />} />
+>>>>>>> Stashed changes:src/routes/Index.tsx
           </Route>
         </Routes>
       </BrowserRouter>
--- src/assets/services/subjectServices.ts
+++ src/assets/services/subjectServices.ts
@@ -24,11 +24,35 @@ export const getSubjects = async (): Promise<ApiResponse<Subject>> => {
     return data;
   } catch (error) {
     console.log("Erro no servidor");
-    return { success: false, message: "" };
+    return { success: false, message: "Erro no servidor" };
   }
 };
 
-export const createSubject = async () => {};
+export const createSubject = async (
+  name: string,
+): Promise<ApiResponse<Subject>> => {
+  const token = localStorage.getItem("token");
+  try {
+    const res = await fetch(`${URL}/subjects`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify({ name }),
+    });
+
+    const data: ApiResponse<Subject> = await res.json();
+
+    if (!data.success) {
+      return { success: false, message: data.message };
+    }
+
+    return data;
+  } catch (error) {
+    return { success: false, message: "Erro no servidor" };
+  }
+};
 
 export const updateSubject = async () => {};
 
